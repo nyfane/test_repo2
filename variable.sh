8e542d5d0e3d5bba1d2c786a87a86379df7c3463
Fanny <jeuboufanny8@gmail.com>
2022-02-21 23:49:01 -0700

    variable

---
+++ variable.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#Author: Fanny
+#Date: 2022-02-21
+#Description:--------script for variable---------
+
+NAME=nyfane
+LAST_NAME=jeubs
+AGE=30
+CLASS=linux
+PROF=serge
+CAL=6
+JOB_NAME=devops engineer
+
+Hi there,
+my name is ${NAME}, my last name is ${LAST_NAME}. 
+I am ${AGE} years old and I am taking ${CLASS} course with prof ${PROF}.
+In ${CAL} I will be looking a job as a ${JOB_NAME}.
+
